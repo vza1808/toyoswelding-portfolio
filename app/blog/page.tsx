7fa8ef3b8ad207080ec27055fcd7fc53a89eccfc
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-02 22:45:48 -0600

    About me title change

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,14 @@
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
-  title: 'Blog',
+  title: 'About Me',
   description: 'Read my blog.',
 }
 
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">About Me</h1>
       <BlogPosts />
     </section>
   )
