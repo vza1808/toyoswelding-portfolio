979b58e28e0e9d7bbe9830ff493215d5858fc99a
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-05 08:58:23 -0600

    About tweak 9 rss fix

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -15,7 +15,7 @@ export async function GET() {
       (post) =>
         `<item>
           <title>${post.metadata.title}</title>
-          <link>${baseUrl}/about/${post.slug}</link>
+          <link>${baseUrl}/blog/${post.slug}</link>
           <description>${post.metadata.summary || ''}</description>
           <pubDate>${new Date(
             post.metadata.publishedAt
