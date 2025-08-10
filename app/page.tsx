593dfb18245f02795c80f295f091127229557a29
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-10 10:57:25 -0600

    Homepage Update 1

--- app/page.tsx
+++ app/page.tsx
@@ -15,11 +15,13 @@ export default function Page() {
         className="rounded-lg mb-6"
       />
       <p className="mb-4">
-        {`I'm Andrew Vazquez, a math enthusiast and first-gen advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm the owner of Toyo's Welding and Repair Services. I focus on providing 
+          high-quality mobile structural welding services in Utah.
+          I’m experienced in structural welding and repairs for commercial buildings as
+          well as steel fabrication projects for the construction industry.
+          I’m skilled in SMAW, GTAW, FCAW, and GMAW welding, and hold certifications in
+           most of these.`
+        }
       </p>
       <div className="my-8">
         <BlogPosts />
