001d6ef1ce45fb5d71f088591884bb6280ca6e22
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-10 17:35:41 -0600

    Homepage Update 9

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Page() {
         <Image
           src="/Toyo.jpeg"
           alt="Metodio Vazquez"
-          width={150}
-          height={150}
+          width={200}
+          height={200}
           className="rounded-lg ml-6"
         />
       </div>
