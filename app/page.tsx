67c97508d4dc5a14bcdc0bf5494b89809cc0090c
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-10 17:32:28 -0600

    Homepage Update 7

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,18 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Metodio Vazquez
       </h1>
-      <Image
-        src="/Toyo.jpeg"
-        alt="Metodio Vazquez"
-        width={200}
-        height={100}
-        className="rounded-lg mb-6"
-      />
-      <p className="mb-4">
-        {`I'm the owner of Toyo's Welding and Repair Services. I focus on providing 
-          high-quality mobile structural welding services in Utah.
-          I’m experienced in structural welding and repairs for commercial buildings as
-          well as steel fabrication projects for the construction industry.
-          I’m skilled in SMAW, GTAW, FCAW, and GMAW welding, and hold certifications in
-           most of these.`
-        }
-      </p>
+      <div className="flex flex-row items-start mb-6">
+        <p className="mb-4 flex-1">
+          I'm the owner of Toyo's Welding and Repair Services. I focus on providing high-quality mobile structural welding services in Utah. I’m experienced in structural welding and repairs for commercial buildings as well as steel fabrication projects for the construction industry. I’m skilled in SMAW, GTAW, FCAW, and GMAW welding, and hold certifications in most of these.
+        </p>
+        <Image
+          src="/Toyo.jpeg"
+          alt="Metodio Vazquez"
+          width={150}
+          height={100}
+          className="rounded-lg ml-6"
+        />
+      </div>
       <div className="my-8">
         <BlogPosts />
       </div>
