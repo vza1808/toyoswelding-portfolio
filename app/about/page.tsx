94aceea327ba4696bd8e74895aaa21e173599483
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-10 22:06:12 -0600

    About Page Updated 1

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
@@ -9,6 +10,13 @@ export default function Page() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">About</h1>
+      <Image
+        src="/DONUT.jpeg"
+        alt="Donut"
+        width={300}
+        height={300}
+        className="rounded-lg mb-6"
+      />
       <BlogPosts />
     </section>
   )
