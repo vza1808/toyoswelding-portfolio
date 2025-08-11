800aecad69f4dd4238eb8412225d9cf3244adcc4
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-11 08:44:41 -0600

    Theme Update 3

--- app/about/[slug]/page.tsx
+++ app/about/[slug]/page.tsx
@@ -86,11 +86,11 @@ export default function Blog({ params }) {
         {post.metadata.title}
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+        <p className="text-sm text-black">
           {formatDate(post.metadata.publishedAt)}
         </p>
       </div>
-      <article className="prose">
+      <article className="prose prose-lg text-black max-w-none">
         <CustomMDX source={post.content} />
       </article>
     </section>
