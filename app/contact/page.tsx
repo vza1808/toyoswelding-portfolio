94e9ee7a6bc18fbc49198d7fff99b958b7c22610
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-11 08:16:22 -0600

    Contact Page Update 3

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,24 @@
+import Image from 'next/image'
+
 export default function ContactPage() {
   return (
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Contact</h1>
+      <Image
+        src="/MOUNTAINS.jpeg"
+        alt="Mountains"
+        width={500}
+        height={300}
+        className="rounded-lg mb-6"
+      />
       <p>
         You can reach me at{' '}
         <a href="mailto:toyovazquez31@gmail.com" className="underline text-blue-600">
           toyovazquez31@gmail.com
         </a>
         , by phone at{' '}
-        <a href="tel:8016879647" className="underline text-blue-600">
-          (801) 687-9647
+        <a href="tel:8019213809" className="underline text-blue-600">
+          (801) 921-3809
         </a>
         , or on{' '}
         <a
