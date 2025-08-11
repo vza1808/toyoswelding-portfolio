2dc4895254e9a7580956c7cc25ffa428b9b91a0e
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-10 22:21:12 -0600

    About Page Updated 5

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
         className="rounded-lg mb-6"
       />
       <div className="mb-6">
-        I am a licensed subcontractor in the State of Utah who performs structural welding in the construction industry. I began learning to weld in my home country, Mexico and continued after immigrating to the United States in 1998. I now have 30+ years of experience in welding both on and off construction sites. My business principally offers services in mobile structural welding as well as the repair of structures (e.g. stairways, panel connections, etc.) for commercial buildings; however, I also work on steel fabrication projects (e.g. giant steel box containers, angle brackets). Over the last six years since my business started, I've been very fortunate to work with some of the most reputable construction companies in Utah.
+        I am a licensed subcontractor in the State of Utah who performs structural welding in the construction industry. I began learning to weld in my home country, Mexico and continued after immigrating to the United States in 1998. I now have 30+ years of experience in welding both on and off construction sites. My business principally offers services in mobile structural welding as well as the repair of structures (e.g. stairways, panel connections, etc.) for commercial buildings. I also work on steel fabrication projects (e.g. giant steel box containers, angle brackets). Over the last 8 years since my business started, I've been very fortunate to work with some of the most reputable construction companies in Utah.
       </div>
     </section>
   )
