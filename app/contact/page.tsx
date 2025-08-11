cedabb05134033bb1962ea46b47d360c09208cfc
Andrew Vazquez <157070667+vza1808@users.noreply.github.com>
2025-08-11 08:09:14 -0600

    Contact Page Update 2

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,24 @@ export default function ContactPage() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Contact</h1>
       <p>
-        You can reach me at <a href="mailto:your@email.com" className="underline text-blue-600">your@email.com</a>.
+        You can reach me at{' '}
+        <a href="mailto:toyovazquez31@gmail.com" className="underline text-blue-600">
+          toyovazquez31@gmail.com
+        </a>
+        , by phone at{' '}
+        <a href="tel:8016879647" className="underline text-blue-600">
+          (801) 687-9647
+        </a>
+        , or on{' '}
+        <a
+          href="https://www.linkedin.com/in/metodio-vazquez-4b7769286/"
+          className="underline text-blue-600"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        .
       </p>
     </section>
   )
